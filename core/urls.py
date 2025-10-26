6be764e788d06725e20d1ce4c0867d3a5356a069
ssali jamil <ssalijamal697@gmail.com>
2025-10-26 06:30:25 +0300

    Added static DIRs

--- core/urls.py
+++ core/urls.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.conf.urls.static import static
+from django.conf import settings
 from voting.api_views import ElectionStatsAPIView
 
 urlpatterns = [
@@ -18,4 +20,8 @@ urlpatterns = [
     # Voting API (Voters + Votes + Election Stats)
     path('api/voting/', include('voting.api_urls')),
     path('api/voting/stats/', ElectionStatsAPIView.as_view(), name='api_election_stats'),
+
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
